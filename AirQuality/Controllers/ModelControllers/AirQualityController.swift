8225f901b4886c0133bd3bb25afe835f3a771d1f
aschemily <aschemily@gmail.com>
2022-02-24 08:51:55 -0500

    initial

--- AirQuality/Controllers/ModelControllers/AirQualityController.swift
+++ AirQuality/Controllers/ModelControllers/AirQualityController.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class AirQualityController{
+    
+    //api key- 13af00b5-02ea-4480-9868-e4943af2e6eb
+    
+    //fetch countries: http://api.airvisual.com/v2/countries?key={{YOUR_API_KEY}}
+
+    //fetch states: http://api.airvisual.com/v2/states?country={{COUNTRY_NAME}}&key={{YOUR_API_KEY}}
+
+    //cities: http://api.airvisual.com/v2/cities?state={{STATE_NAME}}&country={{COUNTRY_NAME}}&key={{YOUR_API_KEY}}
+
+    //city data: http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key={{YOUR_API_KEY}}
+
+    
+    
+    
+}//end of class
